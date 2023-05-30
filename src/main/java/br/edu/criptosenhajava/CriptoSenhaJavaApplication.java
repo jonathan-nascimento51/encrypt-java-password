22a6eb55ba89cd31eab806f9b4bcfcb2e0f8ee87
jonathan-nascimento51 <jonathanmoletta2010@hotmail.com>
2023-05-30 15:24:14 -0300

    feat: add password encryption and validation

--- src/main/java/br/edu/criptosenhajava/CriptoSenhaJavaApplication.java
+++ src/main/java/br/edu/criptosenhajava/CriptoSenhaJavaApplication.java
@@ -3,12 +3,18 @@ package br.edu.criptosenhajava;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)//digo para ele não executar essa classe do spring security
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class CriptoSenhaJavaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CriptoSenhaJavaApplication.class, args);
-	}
+	public static void main(String[] args) { SpringApplication.run(CriptoSenhaJavaApplication.class, args); }
 
+	@Bean
+	public PasswordEncoder getPasswordEnconder() {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		return encoder;
+	}
 }
--- src/main/java/br/edu/criptosenhajava/controller/UserController.java
+++ src/main/java/br/edu/criptosenhajava/controller/UserController.java
@@ -1,13 +1,17 @@
 package br.edu.criptosenhajava.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.criptosenhajava.model.UserModel;
@@ -19,15 +23,37 @@ public class UserController {
 
 	@Autowired
 	private final UserRepository userRepository;
+	private final PasswordEncoder encoder;
 	
-	public UserController(UserRepository userRepository) {  this.userRepository = userRepository; }
-	
-	
+	public UserController(UserRepository userRepository, PasswordEncoder encoder) {  
+		this.userRepository = userRepository; 
+		this.encoder = encoder; 
+	}
+		
 	@GetMapping
 	public ResponseEntity<List<UserModel>> listAll() { return ResponseEntity.ok(userRepository.findAll()); }
 	
 	@PostMapping
-	public ResponseEntity<UserModel> createUser(@RequestBody UserModel user) { return ResponseEntity.ok(userRepository.save(user)); }
+	public ResponseEntity<UserModel> createUser(@RequestBody UserModel user) { 
+		user.setPassword(encoder.encode(user.getPassword()));
+		return ResponseEntity.ok(userRepository.save(user)); 
+	}
+	
+	@GetMapping("/validarSenha")
+	public ResponseEntity<Boolean> validPwd(@RequestParam String login,
+										    @RequestParam String password){
+		
+		Optional<UserModel> optUser = userRepository.findByLogin(login);
+		if (optUser.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(false);
+		}
+		
+		UserModel user = optUser.get();
+		boolean valid = encoder.matches(password, user.getPassword());
+		
+		HttpStatus status = (valid) ? HttpStatus.OK : HttpStatus.UNAUTHORIZED;
+		return ResponseEntity.status(status).body(valid);
+	}
 	
 	
 }
