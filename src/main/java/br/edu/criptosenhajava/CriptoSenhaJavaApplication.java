7098570d7d63fcb93c68bb752bc02dd33478c6f0
jonathan-nascimento51 <jonathanmoletta2010@hotmail.com>
2023-05-30 14:20:46 -0300

    add dependency SpringSecurity

--- src/main/java/br/edu/criptosenhajava/CriptoSenhaJavaApplication.java
+++ src/main/java/br/edu/criptosenhajava/CriptoSenhaJavaApplication.java
@@ -2,8 +2,9 @@ package br.edu.criptosenhajava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)//digo para ele não executar essa classe do spring security
 public class CriptoSenhaJavaApplication {
 
 	public static void main(String[] args) {
