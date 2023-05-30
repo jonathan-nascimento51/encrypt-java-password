b959b5b0755346800dae5ef59ba8773152be9b44
jonathan-nascimento51 <jonathanmoletta2010@hotmail.com>
2023-05-30 13:27:15 -0300

    initialized project

---
+++ src/main/java/br/edu/criptosenhajava/CriptoSenhaJavaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.criptosenhajava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriptoSenhaJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriptoSenhaJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/criptosenhajava/CriptoSenhaJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.criptosenhajava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriptoSenhaJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
